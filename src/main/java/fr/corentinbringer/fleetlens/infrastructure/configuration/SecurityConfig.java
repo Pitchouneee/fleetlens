09334e4a4a21dc3baab3505bfe0edb92eab522d5
Corentin Bringer <corentinbringer@ik.me>
2024-12-23 18:01:56 +0100

    clean SecurityConfig

--- src/main/java/fr/corentinbringer/fleetlens/infrastructure/configuration/SecurityConfig.java
+++ src/main/java/fr/corentinbringer/fleetlens/infrastructure/configuration/SecurityConfig.java
@@ -1,7 +1,6 @@
 package fr.corentinbringer.fleetlens.infrastructure.configuration;
 
 import fr.corentinbringer.fleetlens.domain.model.UserRole;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,7 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
