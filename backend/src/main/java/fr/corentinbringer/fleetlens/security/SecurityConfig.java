ba745c695375792ba5784326f43053e9f56eb8f7
c.bringer <corentin.bringer@ava-funeraire.fr>
2025-09-12 10:05:31 +0200

    conf: cors

--- backend/src/main/java/fr/corentinbringer/fleetlens/security/SecurityConfig.java
+++ backend/src/main/java/fr/corentinbringer/fleetlens/security/SecurityConfig.java
@@ -58,10 +58,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(false);
-        config.setAllowedOriginPatterns(List.of(
-                "http://localhost:5173",
-                "http://127.0.0.1:5173"
-        ));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 
         config.setAllowedHeaders(List.of(
